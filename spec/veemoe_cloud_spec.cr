2d7cb16088811394a8d87af67053756d744e06fe
Hentioe <me@bluerain.io>
2019-12-08 15:48:12 -0400

    Add more tests

--- spec/veemoe_cloud_spec.cr
+++ spec/veemoe_cloud_spec.cr
@@ -51,10 +51,10 @@ describe VeemoeCloud do
     space = Workspace.create!({:name => "test-space", :description => "测试空间"})
     style = Style.create!({:name => "style-1", :description => "无", :workspace_id => space.id})
 
+    style.should be_truthy
+
     style.name.should eq("style-1")
     style.description.should eq("无")
-
-    style.should be_truthy
 
     Style.update!(style, {:name => "updated_style", :description => "更新后的描述。"})
     style.name.should eq("updated_style")
@@ -110,5 +110,75 @@ describe VeemoeCloud do
 
     style2 = Style.find_by_name("style-2")
     style2.should be_nil
+  end
+
+  describe VeemoeCloud::Business::Pipe do
+    begin
+      Pipe.create!({:name => "pipe-1", :query_params => "q-1", :workspace_id => 0})
+    rescue e
+      e.message.not_nil!.should start_with("FOREIGN KEY constraint failed.")
+    end
+    space = Workspace.create!({:name => "test-space", :description => "测试空间"})
+    pipe = Pipe.create!({:name => "pipe-1", :query_params => "q-1", :workspace_id => space.id})
+
+    pipe.should be_truthy
+
+    pipe.name.should eq("pipe-1")
+    pipe.query_params.should eq("q-1")
+
+    Pipe.update!(pipe, {:name => "updated-pipe", :query_params => "updated-q"})
+
+    pipe.name.should eq("updated-pipe")
+    pipe.query_params.should eq("updated-q")
+
+    pipe = Pipe.find_by_name("updated-pipe")
+    pipe.should be_truthy
+
+    Pipe.delete(pipe.not_nil!)
+
+    pipe = Pipe.find_by_name("updated-pipe")
+    pipe.should be_nil
+
+    pipe2 = Pipe.create!({:name => "pipe-2", :query_params => "q-2", :workspace_id => space.id})
+
+    Pipe.find_by_name("pipe-2").should be_truthy
+
+    begin
+      Pipe.create!({:name => "pipe-2", :query_params => "q-2", :workspace_id => space.id})
+    rescue e
+      e.message.not_nil!.should start_with("UNIQUE constraint failed: pipes.workspace_id, pipes.name.")
+    end
+
+    Workspace.delete!(space)
+
+    Pipe.find_by_name("pipe-2").should be_nil
+  end
+
+  describe VeemoeCloud::Business::Match do
+    begin
+      Match.create!({:expression => "/*", :workspace_id => 0})
+    rescue e
+      e.message.not_nil!.should start_with("FOREIGN KEY constraint failed.")
+    end
+    space = Workspace.create!({:name => "test-space", :description => "测试空间"})
+    match = Match.create!({:expression => "/*", :workspace_id => space.id})
+
+    match.should be_truthy
+
+    match.expression.should eq("/*")
+
+    Match.update!(match, {:expression => "/10001/*"})
+
+    match.expression.should eq("/10001/*")
+
+    match2 = Match.create!({:expression => "/10002/*", :workspace_id => space.id})
+
+    begin
+      match2 = Match.create!({:expression => "/10002/*", :workspace_id => space.id})
+    rescue e
+      e.message.not_nil!.should start_with("UNIQUE constraint failed: matches.workspace_id, matches.expression.")
+    end
+
+    Workspace.delete!(space)
   end
 end
