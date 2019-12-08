b314b327f42dd3a30ec55f40923c9d9fd3bc6810
Hentioe <me@bluerain.io>
2019-12-08 10:45:41 -0400

    Implementing Workspace Features and Testing

--- spec/veemoe_cloud_spec.cr
+++ spec/veemoe_cloud_spec.cr
@@ -17,5 +17,32 @@ describe VeemoeCloud do
       demo_space = VeemoeCloud::Business::Workspace.find_by_name("demo")
       demo_space.should be_nil
     end
+
+    space = VeemoeCloud::Business::Workspace.create!({:name => "test-space", :description => "测试空间"})
+    space.should be_truthy
+    space.name.should eq("test-space")
+    space.description.should eq("测试空间")
+
+    begin
+      VeemoeCloud::Business::Workspace.create!({:name => "demo", :description => "将要创建失败的空间。"})
+    rescue e
+      e.message.should eq("Unable to create directory '_res/demo': File exists")
+    end
+
+    space = VeemoeCloud::Business::Workspace.find_by_name("demo")
+    space.should be_falsey
+
+    space = VeemoeCloud::Business::Workspace.find_by_name("test-space")
+    space.should be_truthy
+
+    VeemoeCloud::Business::Workspace.update!(space.not_nil!, {:name => "test-space1"})
+    File.exists?("_res/test-space1").should be_true
+
+    VeemoeCloud::Business::Workspace.delete!("test-space1")
+
+    space = VeemoeCloud::Business::Workspace.find_by_name("test-space1")
+    space.should be_falsey
+
+    File.exists?("_res/test-space1").should be_false
   end
 end
--- src/veemoe_cloud/business/workspace.cr
+++ src/veemoe_cloud/business/workspace.cr
@@ -3,5 +3,37 @@ module VeemoeCloud
     def self.find_by_name(name : String)
       Workspace.where { _name == name }.first
     end
+
+    def self.create!(data : Hash)
+      if data[:is_protected]? == nil
+        data = data.merge({:is_protected => true})
+      end
+
+      Jennifer::Adapter.adapter.transaction do
+        space = Workspace.create!(data)
+        FileUtils.mkdir("_res/#{space.name}")
+        space
+      end.not_nil!
+    end
+
+    def self.update!(space : Workspace, data : Hash)
+      Jennifer::Adapter.adapter.transaction do
+        last_name = space.name
+        space.update_columns(data)
+        if last_name != space
+          FileUtils.mv("_res/#{last_name}", "_res/#{space.name}")
+        end
+        space
+      end.not_nil!
+    end
+
+    def self.delete!(name : String)
+      Jennifer::Adapter.adapter.transaction do
+        if space = find_by_name(name)
+          FileUtils.rm_r("_res/#{space.name}")
+          space.delete
+        end
+      end
+    end
   end
 end
